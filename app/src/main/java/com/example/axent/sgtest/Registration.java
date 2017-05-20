74082606fccf2f169c4a5d63893343475b1afcc1
axemilon <mrdmitriyhr@gmail.com>
2017-05-20 14:05:47 +0300

    email validation
    Реализована валидация email-адреса при регистрации

--- app/src/main/java/com/example/axent/sgtest/Registration.java
+++ app/src/main/java/com/example/axent/sgtest/Registration.java
@@ -9,6 +9,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import org.apache.commons.validator.routines.EmailValidator;
+
 
 
 public class Registration extends Activity {
@@ -43,30 +45,48 @@ public class Registration extends Activity {
                 EditText mail_EditText = (EditText) findViewById(R.id.reg_email_edit);
                 EditText password_EditText = (EditText) findViewById(R.id.reg_password_edit);
                 String mail = mail_EditText.getText().toString();
+                EmailValidator emailValidator = EmailValidator.getInstance();
+                if (mail_EditText.length() > 6) {
+                    if (password_EditText.length() > 4){
+                        if (emailValidator.isValid(mail)) {
+                            if (dbHelper.checkMail(mail) == 0){
+                                SQLiteDatabase database = dbHelper.getWritableDatabase();
+                                ContentValues contentValues = new ContentValues();
+                                contentValues.put(dbHelper.KEY_MAIL, mail_EditText.getText().toString());
+
+                                MD5Crypt md5Crypt = new MD5Crypt();
+                                contentValues.put(dbHelper.KEY_PASSWORD, MD5Crypt.md5Custom(password_EditText.getText().toString()));
+                                database.insert(dbHelper.TABLE_USERS, null, contentValues);
+
+                                Toast succses_mes = Toast.makeText(Registration.this, "Registration sucsessful", Toast.LENGTH_LONG);
+                                succses_mes.show();
+
+                                Intent intent = new Intent(Registration.this, Main.class);
+                                startActivity(intent);
+                                database.close();
+                                finish();
+
+                            } else{
 
 
-                    if (dbHelper.checkMail(mail) > 0) {
-                        Toast error_mes = Toast.makeText(Registration.this, "User already exist, try another", Toast.LENGTH_LONG);
-                        error_mes.show();
+                                Toast error_mes = Toast.makeText(Registration.this, "User already exist, try another", Toast.LENGTH_LONG);
+                                error_mes.show();
 
+                            }
+                        } else {
+                            Toast error_mes = Toast.makeText(Registration.this, "Email not valid", Toast.LENGTH_LONG);
+                            error_mes.show();
+
+                        }
                     } else {
-                        SQLiteDatabase database = dbHelper.getWritableDatabase();
-                        ContentValues contentValues = new ContentValues();
-                        contentValues.put(dbHelper.KEY_MAIL, mail_EditText.getText().toString());
-
-                        MD5Crypt md5Crypt = new MD5Crypt();
-                        contentValues.put(dbHelper.KEY_PASSWORD, MD5Crypt.md5Custom(password_EditText.getText().toString()));
-                        database.insert(dbHelper.TABLE_USERS, null, contentValues);
-
-                        Toast succses_mes = Toast.makeText(Registration.this, "Registration sucsessful", Toast.LENGTH_LONG);
-                        succses_mes.show();
-
-                        Intent intent = new Intent(Registration.this, Main.class);
-                        startActivity(intent);
-                        database.close();
-                        finish();
-
+                        Toast error_mes = Toast.makeText(Registration.this, "Password must be at least 4 characters long", Toast.LENGTH_LONG);
+                        error_mes.show();
                     }
+
+                } else {
+                    Toast error_mes = Toast.makeText(Registration.this, "Email must be at least 6 characters long", Toast.LENGTH_LONG);
+                    error_mes.show();
+                }
 
 
                 }
